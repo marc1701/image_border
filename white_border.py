e23db6a81f6bf3160a2fce5061b8abde9c137031
Marc Green <marc1701@Marcs-MacBook-Air.local>
2022-11-27 11:00:12 +0000

    Initial commit.

---
+++ white_border.py
@@ -0,0 +1,51 @@
+import warnings
+import numpy as np
+from skimage import io
+from skimage.transform import rescale
+
+def main(input_filepath, output_filepath=None, quality=95, **kwargs):
+
+    image = io.imread(input_filepath)
+
+    wb = add_white_border(image, **kwargs)
+
+    if not output_filepath: 
+        output_filepath = f'{input_filepath}_wb.jpg'
+
+    io.imsave(wb, output_filepath, quality=quality)
+    
+
+def add_white_border(image, canvas_size=(2160, 2160), thinnest_border=80, verbose=False):
+    
+    cheight, cwidth = canvas_size
+    imheight, imwidth = image.shape
+
+    # make a white background array
+    canvas = np.ones(canvas_size, dtype=np.uint8) * 255
+
+    # calculate scale factors in either dimension, retain the smallest
+    vscale = (cheight - thinnest_border*2) / imheight
+    hscale = (cwidth - thinnest_border*2) / imwidth
+    rescale_factor = min(vscale, hscale)
+    
+    if rescale_factor > 1:
+        warnings.warn('Image will be scaled up')
+    
+    if rescale_factor != 1:
+        image = rescale(image, rescale_factor, preserve_range=True)
+        if verbose: print(f'Rescaling image by factor {rescale_factor}')
+    else: 
+        if verbose: print('Image not rescaled')
+
+    imheight, imwidth = image.shape
+    vspace = (cheight - imheight) // 2
+    hspace = (cwidth - imwidth) // 2
+
+    # add image to canvas
+    canvas[vspace:vspace+imheight, hspace:hspace+imwidth] = image
+
+    return canvas
+
+#
+# could add an additional option for 'gallery' centering 
+# (e.g. slightly higher than centre)
